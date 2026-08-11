59714ff9b4d96c01986d15896750d2d0a98b3eab
olivierkamali9-blip <olivierkamali9@gmail.com>
2026-08-12 00:08:09 +0200

    Ajout : frontend/src/components/Footer.tsx

---
+++ frontend/src/components/Footer.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer>
+      <img src="logo-g-tech.png" alt="G-Tech Logo" />
+      <p>Powered by G-Tech</p>
+    </footer>
+  );
+};
+
+export default Footer;
