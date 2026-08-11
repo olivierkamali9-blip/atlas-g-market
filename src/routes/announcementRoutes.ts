2cb4528c763c6d462ec4be9298bdffffa2f80832
olivierkamali9-blip <olivierkamali9@gmail.com>
2026-08-11 19:16:38 +0200

    Mise à jour : src/routes/announcementRoutes.ts

--- src/routes/announcementRoutes.ts
+++ src/routes/announcementRoutes.ts
@@ -1,18 +1,18 @@
 import express from 'express';
-import { authenticate } from '../middlewares/authMiddleware';
-import { authorizeModerator } from '../middlewares/authMiddleware';
-import { validateAnnouncement } from '../middlewares/validationMiddleware';
-import { getAnnouncements, getAnnouncementById, updateAnnouncement, deleteAnnouncement } from '../controllers/announcementController';
-import { auditLog } from '../middlewares/auditMiddleware';
+import {
+  getAnnouncementById,
+  getAllAnnouncements,
+  updateAnnouncement,
+  deleteAnnouncement,
+} from '../controllers/announcementController';
+import { authMiddleware } from '../middlewares/authMiddleware';
+import { auditMiddleware } from '../middlewares/auditMiddleware';
 
 const router = express.Router();
 
-// Routes publiques
-router.get('/', getAnnouncements);
 router.get('/:id', getAnnouncementById);
-
-// Routes protégées (nécessitent une authentification)
-router.put('/:id', authenticate, validateAnnouncement, auditLog('UPDATE_ANNOUNCEMENT'), updateAnnouncement);
-router.delete('/:id', authenticate, auditLog('DELETE_ANNOUNCEMENT'), deleteAnnouncement);
+router.get('/', getAllAnnouncements);
+router.put('/:id', authMiddleware, updateAnnouncement);
+router.delete('/:id', authMiddleware, deleteAnnouncement);
 
 export default router;
