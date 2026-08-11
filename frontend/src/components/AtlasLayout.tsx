9e43c551a6f96a5897290612fcf83b67d4799c69
olivierkamali9-blip <olivierkamali9@gmail.com>
2026-08-11 08:32:07 +0200

    Mise à jour : frontend/src/components/AtlasLayout.tsx

--- frontend/src/components/AtlasLayout.tsx
+++ frontend/src/components/AtlasLayout.tsx
@@ -1,23 +1,12 @@
 import React from 'react';
-import { Grid, Container } from '@material-ui/core';
-import AtlasHeader from './AtlasHeader';
-import AtlasFooter from './AtlasFooter';
+import { ThemeProvider } from '@material-ui/core/styles';
+import theme from '../styles/theme';
 
 const AtlasLayout = ({ children }) => {
   return (
-    <Container maxWidth="lg">
-      <Grid container spacing={2}>
-        <Grid item xs={12}>
-          <AtlasHeader />
-        </Grid>
-        <Grid item xs={12}>
-          {children}
-        </Grid>
-        <Grid item xs={12}>
-          <AtlasFooter />
-        </Grid>
-      </Grid>
-    </Container>
+    <ThemeProvider theme={theme}>
+      {children}
+    </ThemeProvider>
   );
 };
 
