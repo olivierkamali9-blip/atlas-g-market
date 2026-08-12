280aaeeb554e3a5f42f26d55bbb538cf5c407e59
olivierkamali9-blip <olivierkamali9@gmail.com>
2026-08-13 01:08:38 +0200

    Ajout : frontend/src/components/Header.tsx

---
+++ frontend/src/components/Header.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header>
+      <nav>
+        <ul>
+          <li><a href="#">Accueil</a></li>
+          <li><a href="#">Produits</a></li>
+          <li><a href="#">Emplois</a></li>
+          <li><a href="#">Services</a></li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
